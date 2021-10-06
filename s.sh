a7ac92aa759d1e1ecbc7ce70561d1bc5f916b917
Jack Maloney <git@jmmaloney4.xyz>
2021-10-05 20:22:03 -0500

    Update s.sh

--- s.sh
+++ s.sh
@@ -1,19 +1,10 @@
 #!/bin/bash
 
-CT_VERSION="3.4.0"
-CT_BINARY="linux_amd64"
-
 YQ_VERSION="v4.13.3"
 YQ_BINARY="yq_linux_amd64"
-
-curl -LOs "https://github.com/helm/chart-testing/releases/download/v${CT_VERSION}/chart-testing_${CT_VERSION}_${CT_BINARY}.tar.gz"
-tar -xvf "./chart-testing_${CT_VERSION}_${CT_BINARY}.tar.gz"
 
 curl -LOs "https://github.com/mikefarah/yq/releases/download/${YQ_VERSION}/${YQ_BINARY}.tar.gz"
 tar -xvf "./${YQ_BINARY}.tar.gz"
 
-for chart in $(./ct --config .github/ct.yaml list-changed)
-do
-    version="$(grep '^version:' "$chart/Chart.yaml" | awk '{print $2}' | awk -F '[.\"]' '{$NF = $NF + 1;} 1' | sed 's/ /./g')"
-    ./${YQ_BINARY} eval --inplace ".version=\"$version\"" "$chart/Chart.yaml"
-done
+version="$(grep '^version:' "charts/$1/Chart.yaml" | awk '{print $2}' | awk -F '[.\"]' '{$NF = $NF + 1;} 1' | sed 's/ /./g')"
+./${YQ_BINARY} eval --inplace ".version=\"$version\"" "charts/$1/Chart.yaml"
