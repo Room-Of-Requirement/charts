0c437983b10a3787a67fd4793a937cec3456fe9e
Jack Maloney <git@jmmaloney4.xyz>
2021-10-05 19:45:54 -0500

    Fix script

--- s.sh
+++ s.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 CT_VERSION="3.4.0"
-CT_BINARY="linux_amd64"
+CT_BINARY="darwin_arm64"
 
 YQ_VERSION="v4.13.3"
-YQ_BINARY="yq_darwin_amd64"
+YQ_BINARY="yq_darwin_arm64"
 
 wget "https://github.com/helm/chart-testing/releases/download/v${CT_VERSION}/chart-testing_${CT_VERSION}_${CT_BINARY}.tar.gz"
 tar -xvf "./chart-testing_${CT_VERSION}_${CT_BINARY}.tar.gz"
@@ -15,7 +15,5 @@ tar -xvf "./${YQ_BINARY}.tar.gz"
 for chart in $(./ct --config .github/ct.yaml list-changed)
 do
     version="$(grep '^version:' "$chart/Chart.yaml" | awk '{print $2}' | awk -F '[.\"]' '{$NF = $NF + 1;} 1' | sed 's/ /./g')"
-    ./${YQ_BINARY} -y \
-      --arg new "$version" \
-      '.version |= $new )' yaml
+    ./${YQ_BINARY} eval --inplace ".version=\"$version\"" "$chart/Chart.yaml"
 done
