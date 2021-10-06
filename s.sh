ced9eb06e8aa1b9b12d5ba7d7732955876a06b21
Jack Maloney <git@jmmaloney4.xyz>
2021-10-05 20:11:02 -0500

    Fix script

--- s.sh
+++ s.sh
@@ -6,10 +6,10 @@ CT_BINARY="linux_amd64"
 YQ_VERSION="v4.13.3"
 YQ_BINARY="yq_linux_amd64"
 
-wget "https://github.com/helm/chart-testing/releases/download/v${CT_VERSION}/chart-testing_${CT_VERSION}_${CT_BINARY}.tar.gz"
+curl -LOs "https://github.com/helm/chart-testing/releases/download/v${CT_VERSION}/chart-testing_${CT_VERSION}_${CT_BINARY}.tar.gz"
 tar -xvf "./chart-testing_${CT_VERSION}_${CT_BINARY}.tar.gz"
 
-wget "https://github.com/mikefarah/yq/releases/download/${YQ_VERSION}/${YQ_BINARY}.tar.gz"
+curl -LOs "https://github.com/mikefarah/yq/releases/download/${YQ_VERSION}/${YQ_BINARY}.tar.gz"
 tar -xvf "./${YQ_BINARY}.tar.gz"
 
 for chart in $(./ct --config .github/ct.yaml list-changed)
