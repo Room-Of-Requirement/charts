b19f1dc837d729457b2a73eca6e4a40d33a6179f
Jack Maloney <jmmaloney4@gmail.com>
2021-10-05 20:02:30 -0500

    Update s.sh

--- s.sh
+++ s.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 CT_VERSION="3.4.0"
-CT_BINARY="darwin_arm64"
+CT_BINARY="linux_amd64"
 
 YQ_VERSION="v4.13.3"
-YQ_BINARY="yq_darwin_arm64"
+YQ_BINARY="yq_linux_amd64"
 
 wget "https://github.com/helm/chart-testing/releases/download/v${CT_VERSION}/chart-testing_${CT_VERSION}_${CT_BINARY}.tar.gz"
 tar -xvf "./chart-testing_${CT_VERSION}_${CT_BINARY}.tar.gz"
