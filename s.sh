8b850ce4195f2e74740ec3adb2eecd5f91e9fddb
Jack Maloney <git@jmmaloney4.xyz>
2021-10-05 20:34:47 -0500

    Fix s

--- s.sh
+++ s.sh
@@ -9,7 +9,7 @@ CT_BINARY="linux_amd64"
 YQ_VERSION="v4.13.3"
 YQ_BINARY="yq_linux_amd64"
 
-curl -Ls "https://raw.githubusercontent.com/helm/helm/main/scripts/get-helm-3" | USE_SUDO=false bash
+curl -Ls "https://raw.githubusercontent.com/helm/helm/main/scripts/get-helm-3" | USE_SUDO=false HELM_INSTALL_DIR=$HOME/bin/ bash
 
 curl -LOs "https://github.com/helm/chart-testing/releases/download/v${CT_VERSION}/chart-testing_${CT_VERSION}_${CT_BINARY}.tar.gz"
 tar -xvf "./chart-testing_${CT_VERSION}_${CT_BINARY}.tar.gz"
@@ -17,7 +17,7 @@ tar -xvf "./chart-testing_${CT_VERSION}_${CT_BINARY}.tar.gz"
 curl -LOs "https://github.com/mikefarah/yq/releases/download/${YQ_VERSION}/${YQ_BINARY}.tar.gz"
 tar -xvf "./${YQ_BINARY}.tar.gz"
 
-for chart in $(./ct --config .github/ct.yaml list-changed)
+for chart in $(PATH=$HOME/bin:$PATH ./ct --config .github/ct.yaml list-changed)
 do
     version="$(grep '^version:' "$chart/Chart.yaml" | awk '{print $2}' | awk -F '[.\"]' '{$NF = $NF + 1;} 1' | sed 's/ /./g')"
     ./${YQ_BINARY} eval --inplace ".version=\"$version\"" "$chart/Chart.yaml"
